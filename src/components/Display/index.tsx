4504dcbfe9112bea10fa3a3722187bc50508df47
Samuel Rodrigues <46876408+sckii@users.noreply.github.com>
2020-08-06 16:46:05 -0300

    update css

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -2,19 +2,19 @@ import React, { useState, FormEvent } from 'react';
 import './styles.css'
 
 import api from '../../api/connection';
-
-import hot from '../../assets/svg/001-sunny.svg';
-
+import morning from '../../styles/img/sun rise.jpg'
+import { url } from 'inspector';
 
 interface DisplayConst {
   
 }
 const Display: React.FC<DisplayConst> = ({}) => {
+
   const API = "NsxS40UXS2JcMmrLAkwkTxamY0lhyQ8x";
 
   const [temperature, setTemperature] = useState(0);
-  const [cityName, setCityName] = useState('rio de janeiro');
-  const [iconName, setIconName] = useState('i')
+  const [cityName, setCityName] = useState('Rio de Janeiro');
+  const [iconName, setIconName] = useState('Sun rise')
 
   async function searchTemperature(e: FormEvent){
     e.preventDefault();
@@ -28,13 +28,10 @@ const Display: React.FC<DisplayConst> = ({}) => {
       console.log(name)
       
       api.get(`currentconditions/v1/${ name }?apikey=${API}&details=false`).then(response => {
-        const [{ WeatherIcon, Temperature:{"Metric":{Value}}}] = response.data;
+        const [{ WeatherText, Temperature:{"Metric":{Value}}}] = response.data;
 
         // const weatherChoose ;
-        if (WeatherIcon > 10) {
-          console.log(WeatherIcon)
-          setIconName(WeatherIcon)
-        } else { setIconName(`0${WeatherIcon}`) }
+        setIconName(WeatherText)
         console.log(Value)
         setTemperature( Value )
       })
@@ -43,23 +40,31 @@ const Display: React.FC<DisplayConst> = ({}) => {
   return (
     <div id='display-page'>
       <article>
-        <div className="informations-container">
+        <div className="informations-container" >
           <div className="informations">
             <form onSubmit={searchTemperature}>
-              <label >
-                <input 
-                  value={cityName}
-                  onChange={(e) => { setCityName(e.target.value) }}
-                  type="text"
-                />
-                <span> City name </span>
+              <div className="input">
+                <label >
+                  <input 
+                    value={cityName}
+                    onChange={(e) => { setCityName(e.target.value) }}
+                    type="text"
+                  />
+                  <span> City name </span>
                 
-              </label>
-              <button type="submit" >Search</button>
+                </label>
+              </div>
+              <div className="button">
+                <button type="submit" >Search</button>
+              </div>
             </form>
           </div>
           <div className='graus'>
-            <img src={`https://developer.accuweather.com/sites/default/files/${iconName}-s.png`} alt="icone do tempo"/><h1>{temperature} °</h1>
+            <img 
+              src={morning} 
+              alt='weather'/>
+            <span className='date'><h2>{cityName}</h2><h4>{iconName}</h4><h1>{temperature} °</h1></span>
+            
           </div>
         </div>
       </article>
